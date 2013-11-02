a55328b33a249b92034a70ec2ea3a1cdd1828393
Takashi Unuma <kijima.m.u@gmail.com>
2013-11-03 01:29:44 +0900

    Update install_mpich2-1.4.1p1-gnu.sh

--- gnu/install_mpich2-1.4.1p1-gnu.sh
+++ gnu/install_mpich2-1.4.1p1-gnu.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+
 export CC=gcc
 export CXX=g++
-export FC=gfortran
 export CPP='gcc -E'
 export CXXCPP='gcc -E'
+export FC=gfortran
+export F77=gfortran
 
-./configure --prefix=/usr/local/mpich2-1.4.1p1-gnu --enable-debuginfo --enable-fast=O3 --with-device=ch3:nemesis 2>&1 | tee configure.log
+./configure --prefix=/usr/local/mpich2-1.4.1p1-gnu --enable-debuginfo --enable-fast=O3 --with-device=ch3:nemesis --with-pm=hydra 2>&1 | tee configure.log
 
 #make 2>&1 | tee make.log
 
