06ac234a483097f8eb267945220811427446d8a3
Takashi Unuma <kijima.m.u@gmail.com>
2013-11-03 01:31:58 +0900

    Update install_mpich2-1.4.1p1-intel.sh

--- intel/install_mpich2-1.4.1p1-intel.sh
+++ intel/install_mpich2-1.4.1p1-intel.sh
@@ -2,9 +2,10 @@
 
 export CC=icc
 export CXX=icpc
-export FC=ifort
 export CPP='icc -E'
 export CXXCPP='icc -E'
+export FC=ifort
+export F77=ifort
 
-./configure --prefix=/home/unuma/usr/local/mpich2-1.4.1p1-intel --enable-debuginfo --enable-fast=O3 --with-device=ch3:nemesis 2>&1 | tee configure.log
+./configure --prefix=/home/unuma/usr/local/mpich2-1.4.1p1-intel --enable-debuginfo --enable-fast=O3 --with-device=ch3:nemesis --with-pm=hydra 2>&1 | tee configure.log
 make 2>&1 | tee make.log
