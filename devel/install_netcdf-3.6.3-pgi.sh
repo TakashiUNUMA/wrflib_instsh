e158dbe891813cd837fb788b2869065494551051
Takashi Unuma <unuma_t@storm.dpri.kyoto-u.ac.jp>
2013-11-11 15:48:21 +0900

    Update for PGI compiler

--- devel/install_netcdf-3.6.3-pgi.sh
+++ devel/install_netcdf-3.6.3-pgi.sh
@@ -4,8 +4,7 @@ export INSTDIR=/home/unuma/usr/local/netcdf-3.6.3-pgi
 
 export CC=gcc
 export CFLAGS=''
-#export CXX=g++
-export CXX='gcc -E'
+export CXX=g++
 export CPPFLAGS='-DNDEBUG -DpgiFortran'
 export FC=pgf90
 export F90=pgf90
@@ -18,19 +17,3 @@ export FFLAGS='-O2 -w -V -Msignextend'
 make 2>&1 | tee make.log
 make check
 make test
-
-
-##### old
-#export FC=pgf90
-#export CC=pgcc
-#export CPP=cpp
-#export CXX=
-#export CPPFLAGS='-DpgiFortran'
-
-#export FC=pgf90
-#export F90=pgf90
-#export FFLAGS="-O2 -w -V"
-#export CC=pgcc
-#export CFLAGS="-O2 -Msignextend -V"
-#export CXX=pgCC
-#export CPPFLAGS="-DNDEBUG -DpgiFortran"
--- pgi/install_netcdf-3.6.3-pgi.sh
+++ pgi/install_netcdf-3.6.3-pgi.sh
@@ -1,28 +1,19 @@
 #!/bin/sh
 
+export INSTDIR=/home/unuma/usr/local/netcdf-3.6.3-pgi
+
+export CC=gcc
+export CFLAGS=''
+export CXX=g++
+export CPPFLAGS='-DNDEBUG -DpgiFortran'
 export FC=pgf90
-export CC=pgcc
-export CPP=cpp
-export CXX=
-export CPPFLAGS='-DpgiFortran'
+export F90=pgf90
+export F95=pgf95
+export F77=pgf77
+export FFLAGS='-O2 -w -V -Msignextend'
 
-#export CC=gcc
-#export CFLAGS=''
-#export CXX=g++
-#export CPPFLAGS='-DNDEBUG -DpgiFortran'
-#export FC=pgf90
-#export F90=pgf90
-#export F95=pgf95
-#export F77=pgf77
-#export FFLAGS='-O2 -w -V -Msignextend'
+./configure --prefix=${INSTDIR} 2>&1 | tee configure.log
 
-#export FC=pgf90
-#export F90=pgf90
-#export FFLAGS="-O2 -w -V"
-#export CC=pgcc
-#export CFLAGS="-O2 -Msignextend -V"
-#export CXX=pgCC
-#export CPPFLAGS="-DNDEBUG -DpgiFortran"
-
-./configure --prefix=/usr/local/netcdf-3.6.3-pgi 2>&1 | tee configure.log
 make 2>&1 | tee make.log
+make check
+make test
