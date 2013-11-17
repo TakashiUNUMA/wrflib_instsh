fb2916af11b1525fc6967e2a88c1fee82094bed2
Takashi Unuma <kijima.m.u@gmail.com>
2013-11-18 01:16:09 +0900

    Update install_hdf5-1.8.7-gnu.sh

--- gnu/install_hdf5-1.8.7-gnu.sh
+++ gnu/install_hdf5-1.8.7-gnu.sh
@@ -4,6 +4,5 @@ export FC=gfortran
 
 ./configure --with-zlib=/usr/local/zlib-1.2.5-gnu/include,/usr/local/zlib-1.2.5-gnu/lib --prefix=/usr/local/hdf5-1.8.7-gnu 2>&1 | tee configure.log
 
-make check 2>&1 | tee makecheck.log
 make 2>&1 | tee make.log
-
+make check 2>&1 | tee makecheck.log
