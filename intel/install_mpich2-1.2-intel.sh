447a7aa65dec23209330d904d369cbe3079b06ca
Takashi Unuma <kijima.m.u@gmail.com>
2013-12-05 10:50:13 +0900

    Update install_mpich2-1.2-intel.sh

--- intel/install_mpich2-1.2-intel.sh
+++ intel/install_mpich2-1.2-intel.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export INSTDIR=/usr/local/mpich2-1.2-intel
+
 export CC=icc
 export CXX=icpc
 export F77=ifort
@@ -7,5 +9,5 @@ export F90=ifort
 export CPP='icc -E'
 export CXXCPP='icc -E'
 
-./configure --prefix=/usr/local/mpich2-1.2-intel --enable-debuginfo --with-device=ch3:nemesis 2>&1 | tee configure.log
+./configure --prefix=${INSTDIR} --enable-debuginfo --with-device=ch3:nemesis 2>&1 | tee configure.log
 make 2>&1 | tee make.log
